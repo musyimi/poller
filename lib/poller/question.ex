6ebcdbc8014526d82b724ada5add26b19bd99462
Musyimi <musyimi13@gmail.com>
2023-04-29 10:59:18 +0300

    added poller data types in choice.ex, poll.ex and question.ex files

---
+++ lib/poller/question.ex
@@ -0,0 +1,18 @@
+defmodule Poller.Question do
+alias Poller.Question
+
+  alias __MODULE__
+
+  defstruct(
+    id: nil,
+    description: nil,
+    choices: []
+  )
+
+  def new(id, description) do
+    %Question{
+      id: id,
+      description: description
+    }
+  end
+end
